16b5b38f51031e0e100a92243f7ea731650262f6
dmojarrot <62604183+dmojarrot@users.noreply.github.com>
2021-09-20 18:50:20 -0500

    Add files via upload

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Diego Mojarro on 20/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
